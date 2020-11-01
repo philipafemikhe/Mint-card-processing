d09865dfd4fa172df5e807894a86086d447b9e51
afemikhephilip <phillip.afemikhe@ikooba.com>
2020-11-01 08:02:07 +0100

    Application setup

---
+++ src/main/java/com/card/mintbank/MintHrApplication.java
@@ -0,0 +1,13 @@
+package com.card.mintbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintHrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MintHrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/card/mintbank/MintHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.card.mintbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MintHrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
